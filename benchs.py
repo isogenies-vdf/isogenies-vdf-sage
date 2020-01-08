c34f891f5ca6a559dbeff60d61a3b33b199b07b1
Simon Masson <simon.masson@loria.fr>
2020-01-08 15:50:29 +0100

    bench tate pairing in the case of no denominator

--- benchs.py
+++ benchs.py
@@ -52,6 +52,29 @@ def bench_tate(E, reps=10):
     print('Our version:\t%s s\t(%s reps).' % (t2, reps))
     return [t1,t2]
 
+def bench_tate_no_denominator(E, reps=10):
+    '''
+    Bench the tate pairing when the point in G_2 is chosen using the twist (in
+    the form (x,iy).
+    '''
+    E_Fp2 = E.to_gfp2()
+    EE = EllipticCurve(E_Fp2.field, [0,E_Fp2.A,0,1,0])
+    p = E_Fp2.field.characteristic()
+    P =  E.point_of_order(N=True, n=0, twist=False, deterministic=False)
+    PP = P.get_coordinates(EE)
+    Q = E.point_of_order(N=True, n=0, twist=True, deterministic=False)
+    QQ = Q.get_coordinates(EE)
+    t1 = cputime()
+    for i in range(reps):
+        PP.tate_pairing(QQ, s.N, k=2)
+    t1 = cputime(t1)
+    print('Sage version:\t%s s\t(%s reps).' % (t1, reps))
+    t2 = cputime()
+    for i in range(reps):
+        pairing.tate(PP, QQ, E, denominator=False)
+    t2 = cputime(t2)
+    print('Our version:\t%s s\t(%s reps).' % (t2, reps))
+    return [t1,t2]
 
 def bench_exponentiation(setup, reps=10):
     F = setup.E0.to_gfp2().field
@@ -67,17 +90,17 @@ def bench_exponentiation(setup, reps=10):
     print('Our version:\t%s s\t(%s reps).' % (t2, reps))
     return [t1,t2]
 
-
 if __name__ == '__main__':
     for key, s in setup.SETUPS.items():
         print('\n==== %s ====' % key)
         # Take a curve defined over Fp2
         P = s.E0.elligator(2)
         E1, _ = s.E0.isogeny_forward([P])
-        print('E1=', E1)
         print('Benching square root function')
         bench_sqrt(s.E0)
         print('Benching tate pairing')
         bench_tate(E1)
+        print('Benching tate pairing without denominator')
+        bench_tate_no_denominator(s.E0)
         print('Benching pairing final exponentiation')
         bench_exponentiation(s)
