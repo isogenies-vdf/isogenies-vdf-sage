97f83d8c73c4af6b03c08ef56bd150a167bea277
Simon Masson <simon.masson@loria.fr>
2020-01-28 18:52:30 +0100

    a function for printing, minor change for benching over Fp

--- benchs.py
+++ benchs.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env sage
 # -*- coding: utf-8 -*-
 
-import setup, curve, point, vdf, pairing
+import setup, curve, point, vdf, pairing, pairing_fp
 from sage.schemes.elliptic_curves.constructor import EllipticCurve
 from sage.misc.misc import cputime
+
+def print_bench(t1, t2, reps):
+    print('Sage version:\t%.5f ms\t(%s reps)' % (t1*10**3, reps))
+    print('Our version:\t%.5f ms\t(%s reps)' % (t2*10**3, reps))
 
 def bench_sqrt(curve, reps=10):
     '''
@@ -16,12 +20,11 @@ def bench_sqrt(curve, reps=10):
     for i in range(reps):
         sqrt_u = list_squares[i].sqrt()
     t1 = cputime(t1)
-    print('Sage version:\t%s s\t(%s reps)' % (t1, reps))
     t2 = cputime()
     for i in range(reps):
         sqrt_u = curve._sqrt(list_squares[i])
     t2 = cputime(t2)
-    print('Our version:\t%s s\t(%s reps).' % (t2, reps))
+    print_bench(t1, t2, reps)
     return [t1,t2]
 
 def bench_tate(E, reps=10):
@@ -41,19 +44,19 @@ def bench_tate(E, reps=10):
     for i in range(reps):
         PP.tate_pairing(QQ, s.N, k=2)
     t1 = cputime(t1)
-    print('Sage version:\t%s s\t(%s reps).' % (t1, reps))
     t2 = cputime()
     for i in range(reps):
         pairing.tate(PP, QQ, E)
     t2 = cputime(t2)
-    print('Our version:\t%s s\t(%s reps).' % (t2, reps))
+    print_bench(t1, t2, reps)
     return [t1,t2]
 
-def bench_tate_no_denominator(E, reps=10):
+def bench_tate_no_denominator(setup, reps=10):
     '''
     Bench the tate pairing when the point in G_2 is chosen using the twist (in
     the form (x,iy).
     '''
+    E = s.E0
     E_Fp2 = E.to_gfp2()
     EE = EllipticCurve(E_Fp2.field, [0,E_Fp2.A,0,1,0])
     p = E_Fp2.field.characteristic()
@@ -65,12 +68,11 @@ def bench_tate_no_denominator(E, reps=10):
     for i in range(reps):
         PP.tate_pairing(QQ, s.N, k=2)
     t1 = cputime(t1)
-    print('Sage version:\t%s s\t(%s reps).' % (t1, reps))
     t2 = cputime()
     for i in range(reps):
-        pairing.tate(PP, QQ, E, denominator=False)
+        pairing_fp.tate(setup, PP, QQ, denominator=False)
     t2 = cputime(t2)
-    print('Our version:\t%s s\t(%s reps).' % (t2, reps))
+    print_bench(t1, t2, reps)
     return [t1,t2]
 
 def bench_exponentiation(setup, reps=10):
@@ -79,12 +81,11 @@ def bench_exponentiation(setup, reps=10):
     for i in range(reps):
         (F.random_element())**((F.characteristic()**2-1)//setup.N)
     t1 = cputime(t1)
-    print('Sage version:\t%s s\t(%s reps).' % (t1, reps))
     t2 = cputime()
     for i in range(reps):
         pairing.exponentiation(setup, F.random_element())
     t2 = cputime(t2)
-    print('Our version:\t%s s\t(%s reps).' % (t2, reps))
+    print_bench(t1, t2, reps)
     return [t1,t2]
 
 if __name__ == '__main__':
@@ -98,6 +99,6 @@ if __name__ == '__main__':
         print('Benching tate pairing')
         bench_tate(E1)
         print('Benching tate pairing without denominator')
-        bench_tate_no_denominator(s.E0)
+        bench_tate_no_denominator(s)
         print('Benching pairing final exponentiation')
         bench_exponentiation(s)
