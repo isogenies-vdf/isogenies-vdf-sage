4dac8d53082d17586c2d1a1c5e3b6e7f2f8e73b5
Simon Masson <simon.masson@loria.fr>
2020-01-29 15:31:34 +0100

    few new tests on line computation and new tate pairing

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env sage
 # -*- coding: utf-8 -*-
 
-import setup, curve, point, vdf, pairing, pairing_fp
+import setup, curve, point, vdf, pairing
 from sage.schemes.elliptic_curves.constructor import EllipticCurve
 
 def test_isog(E):
@@ -21,43 +21,36 @@ def test_vdf(setup, delay, vdfclass=vdf.VDF_GFp, reps=10):
         fQ = vdf.evaluate(Q)
         assert vdf.verify(Q, fQ)
 
-def test_eval_line(E, reps=10):
-    it = E.iterelligator(twist=False, deterministic=False)
-    EE = EllipticCurve(E.field, [0,E.A,0,1,0])
-    i = 0
-    while i<reps:
-        P = next(it)
-        PP = P.get_coordinates(EE)
-        Q = next(it)
-        QQ = Q.get_coordinates(EE)
-        Sum,line = pairing.eval_line(PP, QQ, QQ)
-        assert line[0] == 0
-        assert Sum.curve()(Sum) == PP.curve()(PP) + PP.curve()(QQ)
-        Sum2,line2 = pairing.eval_line(PP, PP, PP)
-        assert line2[0] == 0
-        assert Sum2.curve()(Sum2) == 2*PP.curve()(PP)
-        i+=1
-
-def test_double_line_J_montgomery(E, reps=10):
+def test_double_line_jac(E, reps=10):
     EE = E.weierstrass()
     for i in range(reps):
         S = EE.random_point()
+        while S.is_zero():
+            S = EE.random_point()
         P = EE.random_point()
+        while P.is_zero():
+            P = EE.random_point()
         u = E.field.random_element()
         S_j = (S[0]*u**2, S[1]*u**3, u)
-        [l, dblS] = pairing_fp.double_line_J_montgomery(S_j, P, EE.a2())
-        assert 2*S == EE.point([dblS[0]/dblS[2]**2, dblS[1]/dblS[2]**3])
+        [l, dblS] = pairing.double_line_jac(S_j, P, EE.a2())
+        assert (dblS[2] == 0 and S[1] == 0) or 2*S == EE.point([dblS[0]/dblS[2]**2, dblS[1]/dblS[2]**3])
         assert S._line_(S, P) == l[0]/l[1]
 
-def test_add_line_J_montgomery(E, reps=10):
+def test_add_line_jac(E, reps=10):
     EE = E.weierstrass()
     for i in range(reps):
         S = EE.random_point()
+        while S.is_zero():
+            S = EE.random_point()
         P = EE.random_point()
+        while P.is_zero():
+            P = EE.random_point()
         Q = EE.random_point()
+        while Q.is_zero():
+            Q = EE.random_point()
         u = E.field.random_element()
         S_j = (S[0]*u**2, S[1]*u**3, u)
-        [l, SplusP] = pairing_fp.double_line_J_montgomery(S_j, P, Q, EE.a2())
+        [l, SplusP] = pairing.double_line_jac(S_j, P, Q, EE.a2())
         assert S+P == EE.point([SplusP[0]/SplusP[2]**2, SplusP[1]/SplusP[2]**3])
         assert S._line_(P, Q) == l[0]/l[1]
 
@@ -69,19 +62,17 @@ def test_miller(E, reps=10):
     while i<reps:
         P =  E.point_of_order(N=True, n=0, twist=False, deterministic=False)
         PP = P.get_coordinates(EE)
+        PP_j = (PP[0]*PP[2], PP[1]*PP[2]**2, PP[2])
         Q = E_Fp2.point_of_order(N=True, n=0, twist=False, deterministic=False)
         while Q.x/Q.z in E.field:
             Q = E_Fp2.point_of_order(N=True, n=0, twist=False, deterministic=False)
         QQ = Q.get_coordinates(EE)
         assert (E.setup.N * P).is_zero() and not(P.is_zero())
         assert (E.setup.N * Q).is_zero() and not(Q.is_zero())
-        S1, [n1,d1] = pairing.miller(PP, QQ, s.N, denominator=True)
-        S2, [n2,d2] = pairing.miller(2*PP, QQ, s.N, denominator=True)
-        S3, [n3,d3] = pairing.miller(PP, 2*QQ, s.N, denominator=True)
-        e2PQ = (n2/d2)**((p**2-1)//s.N)
-        eP2Q = (n3/d3)**((p**2-1)//s.N)
-        ePQ2 = (n1/d1)**((p**2-1)//s.N*2)
-        assert e2PQ == eP2Q and eP2Q == ePQ2
+        [l1, S1] = pairing.miller(PP_j, QQ, E.setup.N, EE.a2(), denominator=True)
+        l2 = PP._miller_(QQ, E.setup.N)
+        assert l2 == l1[0]/l1[1]
+        assert S1[2] == 0
         i+=1
 
 def test_sqrt(curve, reps=10):
@@ -99,7 +90,7 @@ def test_exponentiation(setup, reps=10):
 
 def test_tate(E, reps=10):
     E_Fp2 = E.to_gfp2()
-    EE = EllipticCurve(E_Fp2.field, [0,E_Fp2.A,0,1,0])
+    EE = E_Fp2.weierstrass()
     i = 0
     p = E_Fp2.field.characteristic()
     while i<reps:
@@ -109,9 +100,10 @@ def test_tate(E, reps=10):
         while Q.x/Q.z in E.field:
             Q = E_Fp2.point_of_order(N=True, n=0, twist=False, deterministic=False)
         QQ = Q.get_coordinates(EE)
-        assert pairing.tate(PP, 2*QQ, E, denominator=True) == pairing.tate(2*PP, QQ, E, denominator=True)
-        assert pairing.tate(PP, QQ, E, denominator=True)**2 == pairing.tate(2*PP, QQ, E, denominator=True)
+        ePQ = pairing.tate(E.setup, PP, QQ)
+        assert ePQ == PP.tate_pairing(QQ, E.setup.N, k=2)
         i+=1
+
     # Check the pairing computation without denominators
     if E.field.is_prime_field() :
         i = 0
@@ -122,10 +114,9 @@ def test_tate(E, reps=10):
             #while Q.x/Q.z in E.field:
             #    Q = E_Fp2.point_of_order(N=True, n=0, twist=False, deterministic=False)
             QQ = Q.get_coordinates(EE)
-            assert pairing.tate(PP, 2*QQ, E, denominator=True) == pairing.tate(2*PP, QQ, E, denominator=False)
-            assert pairing.tate(PP, QQ, E, denominator=True)**2 == pairing.tate(2*PP, QQ, E, denominator=False)
+            ePQ = pairing.tate(E.setup, PP, QQ, denominator=False)
+            assert ePQ == PP.tate_pairing(QQ, E.setup.N, k=2)
             i+=1
-
 
 if __name__ == '__main__':
     for key, s in setup.SETUPS.items():
@@ -137,15 +128,12 @@ if __name__ == '__main__':
         test_vdf(s, 100)
         print('Testing GF(p²) VDF')
         test_vdf(s, 10, vdf.VDF_GFp2)
-        print('Testing line evaluation')
-        test_eval_line(s.E0)
-        test_eval_line(E1)
         print('Testing double line evaluation in jacobian coordinates')
-        test_double_line_J_montgomery(s.E0)
-        test_double_line_J_montgomery(E1)
+        test_double_line_jac(s.E0)
+        test_double_line_jac(E1)
         print('Testing add line evaluation in jacobian coordinates')
-        test_double_line_J_montgomery(s.E0)
-        test_double_line_J_montgomery(E1)
+        test_double_line_jac(s.E0)
+        test_double_line_jac(E1)
         print('Testing miller loop')
         test_miller(s.E0)
         test_miller(E1)
